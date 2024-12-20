d2e52446c8c1d9b6df1b73f1344c502358727a94
Ashraf Khan <ashrfkhn97@gmail.com>
2024-12-20 20:14:21 +0530

    Eight commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./jamming.module.css";
+import { use } from 'react';
+
+const clientId = '27e5a678ded9406f9eac4d869726a110'; // Replace with your Client ID
+const clientSecret = 'd1a8e945d82c44df84ad6faa7a7f713b';// Replace with your Client Secret
+const redirectUri = "http://localhost:3000/callback"; // Replace with your redirect URI
+const scopes = "user-read-private user-read-email user-library-read";
 
 
 function App() {
@@ -14,62 +20,103 @@ function App() {
   const [showPlaylist, setShowPlaylist] = useState(false);
   const [singerName, setSingerName] = useState('');
   const [url, setUrl] = useState([]);
-  const [loadingOn,setLoadingOn] = useState(false);
-  const [loadingOff,setLoadingOff] = useState(false);
+  const [visble, setVisble] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [newSong, setNewSong] = useState([]);
+  const [Login , SetLogin] = useState(false);
+  const [logOut, setLogOut] = useState(false);
 
-  const [info, setInfo] = useState({
-    heading: '',
-    song: [],
+  const [info, setInfo] = useState(()=> {
+   const saved = localStorage.getItem('savedPlaylist',);
+    return saved ? JSON.parse(saved) : {
+      heading: '',
+      song: [],
+    }
   });
+  const [searchTerm, setNewSearchTerm] = useState("");
+
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('authToken') || '');
+  const [refreshToken, setRefreshToken] = useState('');
+ 
+
+  const [expirationTime, setExpirationTime] = useState(0);
 
 
 
 
   function handleSubmit() {
     
-   setInfo((prev) => {
-    return {
-      
-      
-      ...prev,
-      heading: title,
-      song : addSong,
-     
-    }
-   });
+   setInfo(prev => {return {...prev, 
+    heading:title,
+    song: addSong,
+
+  }});
+   
     setSong((prev) => {return {
       ...prev, song: addSong
     }});
     
+   
+
  
     
     if(title.length !==0 && song.song.length !== 0 ) {
+      setLoading(true);
+      
       setPlaylistName(title);
       setToggle(true);
+
       setAddSong([]);
       setTitle('');
-      setLoadingOn(true);
-      setLoadingOff(false);
+     setVisble(false);
+      setPlaylist([]);
+      localStorage.setItem('savedPlaylist', JSON.stringify(info));
     } else {
 
     setToggle(false);
     return;
     }
-   setTimeout(()=> {
-    setLoadingOn(false);
-    setLoadingOff(true);
-   },2000);
+
+     
+    setTimeout(() => {
+      
+      setVisble(true);
+    }, 1000);
+
+    setTimeout(() => {
+      
+      setLoading(false);
+    }, 3000);
    
 
+    
+  
+    console.log(info);
   }
 
 
+
+useEffect(()=> {
+  setVisble(true);
+  setToggle(true);
+  setShowPlaylist(true);
+  setInput(localStorage.getItem('searchTerm'));
+  
+  console.log(info);
+
+},[])
+
+useEffect(()=> {
+  
+  
+}, [info]);
+
   function addPlay(i) {
     setAddSong((prev) => {
-      if (prev.includes(playlist[i])) {
+      if (prev.includes(newSong[i])) {
         return [...prev];
       } else {
-        return [playlist[i], ...prev]
+        return [newSong[i], ...prev]
       }
     });
   }
@@ -80,7 +127,7 @@ function App() {
     setAddSong(addSong.filter((item, index) => index !== i));
   }
 
-
+  
   const track = addSong.map((song, index) => {
     return (
       <>
@@ -132,49 +179,149 @@ function App() {
     </div>
   </div> );
 
+
+const handleLogin = () => {
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+  window.location.href = authUrl;
+  
+};
+const handleLogout = () => {
+  setAccessToken('');
+  localStorage.removeItem('authToken');
+  SetLogin(true);
+};
+
+useEffect(()=> {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+
+  if(code) {
+    fetchAccessToken(code);
+  }
+
+},[accessToken]);
+
+const fetchAccessToken = async (code) => {
+  const url = "https://accounts.spotify.com/api/token";
+  const bodyParams = new URLSearchParams({
+    grant_type: "authorization_code",
+    code: code, // Authorization code from URL
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    client_secret: clientSecret,
+  });
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      
+    },
+    body: bodyParams.toString(),
+  };
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      const errorDetails = await response.text();
+      console.error(`Failed to fetch access token: ${response.status}`, errorDetails);
+      return;
+    }
+    const data = await response.json();
+    if (data.access_token) {
+      setAccessToken(data.access_token);
+      localStorage.setItem('authToken', data.access_token);
+  } else {
+      console.log('Login failed');
+  }
+    console.log("Token response data:", data);
+    
+    // setAccessToken(data.access_token);
+    setRefreshToken(data.refresh_token);
+    setExpirationTime(Date.now() + data.expires_in *1000 );
+    scheduleTokenRefresh(data.expires_in - 60);
+  } catch (error) {
+    console.error("Error fetching access token:", error);
+  }
+};
+
+
+const refreshAccessToken = async () => {
+  try {
+    const url = "https://accounts.spotify.com/api/token";
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      client_id: clientId
+    }),
+  }
+  const response = await fetch(url, payload);
+  const data = await response.json();
+    setAccessToken(data.access_token);
+    setExpirationTime(Date.now()+ data.expires_in * 1000 );
+
+    scheduleTokenRefresh(expirationTime- 60);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+const scheduleTokenRefresh = (delayInSeconds) => {
+  setTimeout(() => {
+    refreshAccessToken();
+  }, delayInSeconds * 1000);
+};
+
+
+
   const Song = async (event) => {
-    event.preventDefault();
-
-    const url = `https://spotify23.p.rapidapi.com/search/?q=${input}&type=tracks&offset=0&limit=10&numberOfTopResults=5`;
-
-    const options = {
+  event.preventDefault();
+    localStorage.setItem('searchTerm',input);
+    const url = `https://api.spotify.com/v1/search?q=${input}&type=track&include_external=audio&limit=20`;
+    
+    
+    const authorizationSearch = {
       method: 'GET',
       headers: {
-        'x-rapidapi-key': '15196b2134msha154160161c7b77p1f66c0jsn0acdab18522f',
-        'x-rapidapi-host': 'spotify23.p.rapidapi.com'
+        'Content-Type': 'application/json', // Fixed incorrect 'applications/json' to 'application/json'
+        "Authorization": `Bearer ${accessToken}` // Added a space after 'Bearer' and used template literals for clarity
       }
-    }
+    };
+  
     try {
-      const response = await fetch(url, options)
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        console.log(jsonResponse);
-        const query = jsonResponse.query;
-        const artist = jsonResponse.tracks.items[0].data.artists.items[0].profile.name;
-        const res = jsonResponse.tracks.items;
-      
-        setSingerName(artist);
-        const songUrl = res[0].data.albumOfTrack.sharingInfo.shareUrl;
-        console.log(songUrl);
-           if (playlist.length < 10) {
-          for (let i = 0; i < Math.min(res.length, 10); i++) {
-            setPlaylist((prev) => {
-              return [res[i].data.name, ...prev];
-            }
-            )
-            setUrl((prev )=> {
-              return [res[i].data.albumOfTrack.sharingInfo.shareUrl, ...prev]
+      const res = await fetch(url, authorizationSearch);
+    
+      console.log()
 
-            });
-          }
-        }
-
-
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
       }
+  
+      const data = await res.json(); // Awaited the response JSON parsing
+      console.log(data.tracks.items.length);
+        for(let i =0; i <data.tracks.items.length; i++ ) {
+          setPlaylist(prev => [data.tracks.items[i].name, ...prev]);
+        }
+        setNewSong(playlist.filter(s => !info.song.some(val => val === s)));
+       
+        
+         
+          
+       
+      // console.log(playlist.filter(s => info.song.every(val => val !== s)));
+      // console.log(data.tracks.items);
     } catch (error) {
-      console.log(error);
+      console.error('Error fetching song:', error);
     }
-    setInput('');
+    
+
   }
 
 
@@ -182,6 +329,9 @@ function App() {
     <>
    
       <div className={styles.heading}><h1>Jamming</h1>
+    {!Login && <button onClick={handleLogin}>Login to spotify</button>}
+      {logOut && <button onClick={handleLogout}>Logout to Spotify</button> }
+
       </div>
 
       <div className={styles.searchContainer} >
@@ -197,7 +347,7 @@ function App() {
         <div className={styles.firstbox}>
 
           <h2 >Results</h2>
-          {playlist.map((tracks, index) => {
+          {newSong.slice(0,10).map((tracks, index) => {
             return (
               <>
               <div className={styles.innerBoxOne}>
@@ -207,7 +357,7 @@ function App() {
                   
                     <li >{singerName}</li>
                    <li>
-                    <audio src={url[0]} />
+                    
                    </li>
                   </ul>
                 </div>
@@ -241,13 +391,16 @@ function App() {
           
          
         </div>
-       
-        {loadingOn ? <p>Paylist Being Saved....</p>: null}
-        {loadingOff ? savedPlaylist: null}
+        {visble && <div>
+          {loading ? <p>Paylist Being Saved....</p> : savedPlaylist } 
+          </div>}
+        
+        
+        
       </div>
 
 
-
+      
 
 
     </>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+
+// import SpotifySongSearch from './spotifysongsearch';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <SpotifySongSearch /> */}
     <App />
   </React.StrictMode>
 );
